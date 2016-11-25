c0b497d3f3f2d99675523651ec547784d7e83dcc
Jose Ocampo <davidocampo101@gmail.com>
2016-11-24 19:09:48 -0500

    Habiente de desarrollo terminado

--- scripts/directives.js
+++ scripts/directives.js
@@ -79,4 +79,160 @@
             templateUrl: "views/pageP13.html"
         };
     });
+    app.directive("pageP14",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP14.html"
+        };
+    });
+    app.directive("pageP15",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP15.html"
+        };
+    });
+    app.directive("pageP16",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP16.html"
+        };
+    });
+    app.directive("pageP17",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP17.html"
+        };
+    });
+    app.directive("pageP18",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP18.html"
+        };
+    })
+    ;app.directive("pageP19",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP19.html"
+        };
+    });
+    app.directive("pageP20",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP20.html"
+        };
+    });
+    app.directive("pageP21",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP21.html"
+        };
+    });
+    app.directive("pageP22",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP22.html"
+        };
+    });
+    app.directive("pageP23",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP23.html"
+        };
+    });
+    app.directive("pageP24",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP24.html"
+        };
+    });
+    app.directive("pageP25",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP25.html"
+        };
+    });
+    app.directive("pageP26",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP26.html"
+        };
+    });
+    app.directive("pageP27",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP27.html"
+        };
+    });
+    app.directive("pageP28",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP28.html"
+        };
+    });
+    app.directive("pageP29",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP29.html"
+        };
+    });
+    app.directive("pageP30",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP30.html"
+        };
+    });
+    app.directive("pageP31",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP31.html"
+        };
+    });
+    app.directive("pageP32",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP32.html"
+        };
+    });
+    app.directive("pageP33",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP33.html"
+        };
+    });
+    app.directive("pageP34",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP34.html"
+        };
+    });
+    app.directive("pageP35",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP35.html"
+        };
+    });
+    app.directive("pageP36",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP36.html"
+        };
+    });
+    app.directive("pageP37",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP37.html"
+        };
+    });
+    app.directive("pageP38",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP38.html"
+        };
+    });
+    app.directive("pageP39",function () {
+        return{
+            restrict: 'E',
+            templateUrl: "views/pageP39.html"
+        };
+    });
 })();
